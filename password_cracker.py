043d969891a06ad19fcb0b21fd6d5d61241c9209
danzel-py <67574872+danzel-py@users.noreply.github.com>
2021-07-18 18:07:26 +0900

    first and last commeth

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,23 @@
 import hashlib
 
-def crack_sha1_hash(hash):
-    
+def crack_sha1_hash(hash = "", use_salts = False):
+    pfile = open("top-10000-passwords.txt", "r", encoding='utf-8')
+    for x in pfile:
+        pw = x.strip()
+        if use_salts:
+            sfile = open("known-salts.txt","r",encoding='utf-8')
+            for y in sfile:
+                newpw = pw+y.strip()
+                if hashlib.sha1(str(newpw).encode('utf-8')).hexdigest() == hash:
+                    return pw
+                newpw = y.strip()+pw
+                if hashlib.sha1(str(newpw).encode('utf-8')).hexdigest() == hash:
+                    return pw
+                
+        else:
+            if hashlib.sha1(str(pw).encode('utf-8')).hexdigest() == hash:
+                return pw
+
+
+    return "PASSWORD NOT IN DATABASE"
+
